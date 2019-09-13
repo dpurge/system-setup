c034907a4fc116aa66b737af41aa64c2f2f72635
David P <david.purge@gmail.com>
2019-09-13 02:47:35 +0200

    Install Translit

--- src/install/InstallBaseSystemTools.task.ps1
+++ src/install/InstallBaseSystemTools.task.ps1
@@ -122,4 +122,36 @@ Task InstallBaseSystemTools `
         }
         
     }
+
+    $ApplicationDir = Join-Path -Path $ProgramDir -ChildPath 'VSCode'
+    if (Test-Path -Path $ApplicationDir -PathType Container) {
+        Write-Message -Install -Type Info -Message "VSCode directory already exists: ${ApplicationDir}"
+    } else {
+        $InstallExe = Join-Path -Path $TempDir -ChildPath 'VSCodeSetup-x64-1.38.1.exe'
+        if (-not (Test-Path -Path $InstallExe)) {
+            Write-Message -Install -Type Error -Message "Missing VSCode installation media: ${InstallExe}"
+        } else {
+            Write-Message -Install -Type Info -Message "Installing VSCode into: ${ApplicationDir}"
+            Exec {
+                & $InstallExe /VERYSILENT /NORESTART /DIR "`"${ApplicationDir}`""
+            }
+        }
+        
+    }
+
+    $ApplicationDir = Join-Path -Path $ProgramDir -ChildPath 'Translit'
+    if (Test-Path -Path $ApplicationDir -PathType Container) {
+        Write-Message -Install -Type Info -Message "Translit directory already exists: ${ApplicationDir}"
+    } else {
+        $ZipFile = Join-Path -Path $TempDir -ChildPath 'Universal_Transliteration_v1.0.zip'
+        if (-not (Test-Path -Path $ZipFile)) {
+            Write-Message -Install -Type Error -Message "Missing Translit installation media: ${ZipFile}"
+        } else {
+            Write-Message -Install -Type Info -Message "Installing Translit into: ${ApplicationDir}"
+            Exec {
+                & $SevenZip x "`"${ZipFile}`"" -o"`"${ProgramDir}`""
+            }
+        }
+        
+    }
 }
