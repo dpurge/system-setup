71eb3c2cdf2f48b01752c25297384dce298373c2
David P <david.purge@gmail.com>
2021-07-04 03:52:58 +0200

    Add dir

--- src/application/VisualStudio.ps1
+++ src/application/VisualStudio.ps1
@@ -2,13 +2,13 @@
     Name = 'VisualStudio'
     Verify = @{
         Directory = @(
-            "xxx"
+            "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Community"
         )
     }
     Install = @{
         Uri = 'https://download.visualstudio.microsoft.com/download/pr/9dc321fd-8a9b-47ef-98a9-af0515e08d6f/533c91be0b61f481cb93758a3c6af09cef719cedbaa6e9916232918649fd4a35/vs_Community.exe'
         Media = Join-Path -Path $TempDir -ChildPath 'vs_Community.exe'
-        Directory = "xxx"
+        Directory = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Community"
         Script = {
             param($InstallMedia, $Directory)
             & $InstallMedia `
